66260926c8fd50635494fcf788338a592502afc7
neha-gupta01 <nehagupta96365@gmail.com>
2023-10-16 16:52:04 +0530

    pattern 4

--- index.js
+++ index.js
@@ -69,3 +69,138 @@ while (i >= 1) {
   i--;
 }
 document.getElementById("whilePattern2").textContent = whilePattern2;
+
+//do-while loop
+let doWhilePattern2 = "";
+i = n;
+do {
+  let row = "";
+  let j = 1;
+  do {
+    row += i;
+    j++;
+  } while (j <= i);
+  doWhilePattern2 += row + "\n";
+  i--;
+} while (i >= 1);
+document.getElementById("doWhilePattern2").textContent = doWhilePattern2;
+
+//Pattern 3
+//For loop
+let forPattern3 = "";
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= n - i; j++) {
+    row += " ";
+  }
+  for (let k = 1; k <= i; k++) {
+    row += "*";
+  }
+  forPattern3 += row + "\n";
+  //console.log(row);
+  document.getElementById("forPattern3").textContent = forPattern3;
+}
+
+//While loop
+let whilePattern3 = "";
+i = 1;
+while (i <= n) {
+  let row = "";
+  let j = 1;
+  while (j <= n - i) {
+    row += " ";
+    j++;
+  }
+  let k = 1;
+  while (k <= i) {
+    row += "*";
+    k++;
+  }
+  whilePattern3 += row + "\n";
+  i++;
+}
+document.getElementById("whilePattern3").textContent = whilePattern3;
+
+//do-while loop
+let doWhilePattern3 = "";
+i = 1;
+do {
+  let row = "";
+  let j = 1;
+  do {
+    row += " ";
+    j++;
+  } while (j <= n - i);
+  let k = 1;
+  do {
+    row += "*";
+    k++;
+  } while (k <= i);
+  doWhilePattern3 += row + "\n";
+  i++;
+} while (i <= n);
+document.getElementById("doWhilePattern3").textContent = doWhilePattern3;
+
+//Pattern 4
+//For loop
+let forPattern4 = "";
+let charCode = 65;
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j < i; j++) {
+    row += " ";
+  }
+  for (let k = 1; k <= n - i + 1; k++) {
+    row += String.fromCharCode(charCode);
+    charCode++;
+  }
+  forPattern4 += row + "\n";
+  //console.log(row);
+  document.getElementById("forPattern4").textContent = forPattern4;
+}
+
+//While loop
+let whilePattern4 = "";
+i = 1;
+charCode = 65;
+while (i <= n) {
+  let row = "";
+  let j = 1;
+  while (j < i) {
+    row += " ";
+    j++;
+  }
+  let k = 1;
+  while (k <= n - i + 1) {
+    row += String.fromCharCode(charCode);
+    charCode++;
+    k++;
+  }
+  whilePattern4 += row + "\n";
+  i++;
+}
+document.getElementById("whilePattern4").textContent = whilePattern4;
+
+//do-while loop
+let doWhilePattern4 = "";
+i = 1;
+charCode = 65;
+do {
+  let row = "";
+  let j = 1;
+  do {
+    row += " ";
+    j++;
+  } while (j <= i);
+  let k = 1;
+  do {
+    row += String.fromCharCode(charCode);
+    charCode++;
+    k++;
+  } while (k <= n -i + 1);
+  doWhilePattern4 += row + "\n";
+  i++;
+} while (i <= n);
+document.getElementById("doWhilePattern4").textContent = doWhilePattern4;
+
+
